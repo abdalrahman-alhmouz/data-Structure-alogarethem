36efed8761b5ee86c7e802ebcadb3c156fbcda9a
Abdalrahman jawad alhmouz <75928415+abdalrahman-alhmouz@users.noreply.github.com>
2021-04-28 12:29:56 +0300

    Update LibraryTest.java

--- chalenges/tree/lib/src/test/java/tree/LibraryTest.java
+++ chalenges/tree/lib/src/test/java/tree/LibraryTest.java
@@ -8,6 +8,36 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
 
+
+    @Test public void preOrderTest() {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.rigth = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.rigth = new Node(5);
+//        System.out.println(tree.printPreorder());
+        assertEquals("null1 2 4 5 3 ", tree.printPreorder());
+    }
+    @Test public void inorderTest() {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.rigth = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.rigth = new Node(5);
+        assertEquals("null4 2 5 1 3 ", tree.printInorder());
+    }
+    @Test public void postorderTest() {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.rigth = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.rigth = new Node(5);
+        assertEquals("null4 5 2 3 1 ", tree.printPostorder());
+    }
+
     @Test public void addTest() {
         Node root=new Node(7,null,null);
 
@@ -20,34 +50,6 @@ public class LibraryTest {
         tree.addTow(nodeTow,tree.root);
         assertEquals(9, tree.root.rigth.value);
         assertEquals(1, tree.root.left.value);
-    }
-    @Test public void preOrderTest() {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.rigth = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.rigth = new Node(5);
-//        System.out.println(tree.printPreorder());
-        assertEquals("null1 2 4 5 3 ", tree.printPreorder());
-    }
-    @Test public void inorderTest() {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.rigth = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.rigth = new Node(5);
-        assertEquals("null4 2 5 1 3 ", tree.printInorder());
-    }
-    @Test public void postorderTest() {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.rigth = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.rigth = new Node(5);
-        assertEquals("null4 5 2 3 1 ", tree.printPostorder());
     }
 
     @Test public void instantiateTest() {
