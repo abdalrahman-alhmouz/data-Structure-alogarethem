b05cf8c89c0f118d1f5cc39b33ef4beff5e77398
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-05-20 18:16:38 +0300

    init comit

---
+++ chalenges/hashtable/app/src/main/java/hashtable/HashNode.java
@@ -0,0 +1,15 @@
+package hashtable;
+
+public class HashNode {
+
+    String key;
+    int value;
+
+    public HashNode(String key, int value) {
+        this.key = key;
+        this.value = value;
+
+    }
+
+
+}
